5ca180d6bd9a72bde975ef1e33e661e156cebd4b
Dalal_Hussain_Mohammad <d.h.safar2@gmail.com>
2022-08-10 16:24:20 +0300

    cw2

--- cw2/Assets/xcript.cs
+++ cw2/Assets/xcript.cs
@@ -5,36 +5,41 @@ using UnityEngine;
 
 public class xcript : MonoBehaviour
 {
+
+    string heroName = "Batman";
+    string heroPower = "Flying";
+    int heroAge = 25;
+
+    string villainName = "Joker";
+    string villainPower = "Running away";
+    int villainAge = 32;
     // Start is called before the first frame update
     void Start()
     {
-        
+
+
+        if (heroAge > villainAge)
+        {
+            print(heroName + "is older.");
+        }
+        else if (heroAge == villainAge)
+        {
+            print(heroName + " age eqauls to" + villainName + "age.");
+        }
+        else
+        {
+            print(villainName + " is older.");
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-}
 
-string heroName = "Batman";
-string heroPower = "Flying";
-int heroAge = 25;
 
-string villainName = "Joker";
-string villainPower = "Running away";
-int villainAge = 32;
 
-if (heroAge > villainAge)
-{
-    print(heroName + "is older.");
-}
-else if (heroAge == villainAge)
-{
-    print(heroName + " age eqauls to" + villainName + "age.");
-}
-else
-{
-    print(villainName + " is older.");
+
 }
